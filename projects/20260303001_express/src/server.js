b0053205a534e0d5829cfeecead7ab6de58c344d
João Laranjeira <77790402+jpclrocha@users.noreply.github.com>
2026-03-03 11:10:47 -0300

    entendendo os tipos de middleware

--- projects/20260303001_express/src/server.js
+++ projects/20260303001_express/src/server.js
@@ -3,10 +3,26 @@ import produtosRouter from "./routes/produtos.routes.js";
 import usuariosRouter from "./routes/usuarios.routes.js";
 
 const app = express();
+
+// Middleware 1, logs (middleware de aplicacao)
+app.use((req, res, next) => {
+   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+
+})
+
+// Middleware 2, json parser (middleware de aplicacao)
 app.use(express.json())
 
+// Middleware 3 e 4, rotas (middleware de rota)
 app.use('/usuarios', usuariosRouter);
-
 app.use("/produtos", produtosRouter)
+
+// Middleware de erro — DEVE ter 4 parâmetros
+app.use((err, req, res, next) => {
+   console.error(err.stack);
+   res.status(err.statusCode || 500).json({
+      erro: err.message || 'Erro interno do servidor',
+   });
+});
 
 app.listen(3000);
